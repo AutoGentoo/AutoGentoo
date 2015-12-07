6481a5271e6091342e96d69385afa827f9b964ed
Kronos <dovakhiin1359@gmail.com>
2015-12-06 20:25:37 -0500

    Emerge tests

---
+++ src/install/Emerge.cpp
@@ -0,0 +1,34 @@
+/*
+ * Emerge.hpp
+ * 
+ * Copyright 2015 Andrei Tumbar <atadmin@Helios>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+#include "emerge.hpp"
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	Emerge test(argv[1], "", false);
+	cout << test.Packages[0].properties._updating << "EOL" << endl;
+	return 0;
+}
