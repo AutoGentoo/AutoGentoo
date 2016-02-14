ac52c31b254a5da7bef2e6575a5e0ce2eea936b0
Kronos3 <dovakhiin1359@gmail.com>
2016-02-14 18:13:24 -0500

    Starts to the Kconfig parse

---
+++ src/kernel/parse_kconfig/commands.hpp
@@ -0,0 +1,43 @@
+﻿/*
+* commands.hpp
+*
+* Copyright 2016 Andrei Tumbar <atuser@Kronos>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+*
+*
+*/
+
+#ifndef __AUTOGENTOO_KERNEL_OPERATOR__
+#define __AUTOGENTOO_KERNEL_OPERATOR__
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string __kernel_command__(string str, bool _indent = false)
+{
+	vector <string> cmd_list;
+	cmd_list.push_back("bool");
+	cmd_list.push_back("tristate");
+	cmd_list.push_back("string");
+	cmd_list.push_back("hex");
+	cmd_list.push_back("int");
+};
+
+#endif
---
+++ src/kernel/parse_kconfig/kconfig_module.hpp
@@ -0,0 +1,84 @@
+/*
+* kconfig_module.hpp
+*
+* Copyright 2016 Andrei Tumbar <atuser@Kronos>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+*
+*
+*/
+
+#ifndef __AUTOGENTOO_KCONFIG_MODULE__
+#define __AUTOGENTOO_KCONFIG_MODULE__
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../kernel.hpp"
+#include "../../src/portage/_misc_tools.hpp"
+#include "kernel_tools.hpp"
+
+using namespace std;
+
+template < class T >
+class __KCONFIG_MODULE__
+{
+public:
+	vector <KERNEL_MODULE> selects;
+	vector <NEW_MODULE> depends;
+	string type;
+	string description;
+	T default;
+
+	int feed(vector<string> input)
+	{
+		if (input[0].substr(0, 6) != "config")
+		{
+			return 1;
+		}
+	}
+};
+
+
+/*Example __KCONFIG_MODULE
+config GENTOO_LINUX_UDEV
+	bool "Linux dynamic and persistent device naming (userspace devfs) support"
+	
+	depends on GENTOO_LINUX
+	default y if GENTOO_LINUX
+	
+	select DEVTMPFS
+	select TMPFS
+	
+	select MMU
+	select SHMEM
+	
+	help
+		In order to boot Gentoo Linux a minimal set of config settings needs to
+		be enabled in the kernel; to avoid the users from having to enable them
+		manually as part of a Gentoo Linux installation or a new clean config,
+		we enable these config settings by default for convenience.
+		
+		Currently this only selects TMPFS, DEVTMPFS and their dependencies.
+		TMPFS is enabled to maintain a tmpfs file system at /dev/shm, /run and
+		/sys/fs/cgroup; DEVTMPFS to maintain a devtmpfs file system at /dev.
+		
+		Some of these are critical files that need to be available early in the
+		boot process; if not available, it causes sysfs and udev to malfunction.
+		
+		To ensure Gentoo Linux boots, it is best to leave this setting enabled;
+		if you run a custom setup, you could consider whether to disable this.
+*/
