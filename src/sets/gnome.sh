a0d1a123d067ff61a0148cd3251f9594f4a6a569
Kronos3 <dovakhiin1359@gmail.com>
2016-06-25 16:35:55 -0400

    Removed every for rewrite

--- src/sets/gnome.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-ln -sf /proc/self/mounts /etc/mtab
-systemctl enable dhcpcd.service
-if [ -d /etc/portage/package.use ]
-then
-	cp gnome /etc/portage/package.use/
-else
-	cp gnome /etc/portage/package.use/gnome
--- src/sets/kde.sh
+++
@@ -1,6 +0,0 @@
-cp kdm /etc/conf.d/xdm
-if [ $1 -eq "rc" ]
-then
-	rc-update add xdm default
-elif [ $1 -eq "systemd" ]
-	systemctl enable kdm
