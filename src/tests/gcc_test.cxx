53f703f54e25ad1ea2c3db7c2ce863658023e717
Kronos3 <dovakhiin1359@gmail.com>
2016-07-20 17:08:44 -0400

    Added tests (wont work)

---
+++ src/tests/gcc_test.cxx
@@ -0,0 +1,41 @@
+/*
+ * gcc_test.cxx
+ * 
+ * Copyright 2016 Andrei Tumbar <atadmin@Helios>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+#include "../package/package.hpp"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	//EmergePackage gcc ( "[ebuild   R    ] sys-devel/gcc-4.9.3:4.9::gentoo  USE=\"cxx fortran (multilib) nls nptl openmp sanitize (-altivec) (-awt) -cilk -debug -doc (-fixed-point) -gcj -go -graphite (-hardened) (-libssp) (-multislot) -nopie -nossp -objc -objc++ -objc-gc -regression-test -vanilla\" 87,940 KiB" );
+	//cout << gcc.flags_str["USE"] << endl;
+	//cout << gcc.flags["USE"][1] << endl;
+	//cout << gcc.path ( ) << endl;
+	//map<string, string> vars = get_command ( "lscpu" );
+	string pkg ( argv[1] );
+	Package package ( pkg );
+	cout << package.file << endl;
+	return 0;
+}
---
+++ src/tests/package_text.cxx
@@ -0,0 +1,39 @@
+/*
+ * package_text.cxx
+ * 
+ * Copyright 2016 Andrei Tumbar <atuser@Kronos>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+//#include "../tools/_misc_tools.hpp"
+//#include "../package/package.hpp"
+
+int main(int argc, char **argv)
+{
+	//Package ftp ( "[ebuild   R    ] net-ftp/oftpd-0.3.7-r7::gentoo  USE=\"ipv6\"");
+	//std::cout << ftp.old << std::endl;
+	
+	bool a = true;
+	a = !a;
+	std::cout << a << std::endl;
+	return 0;
+}
+
