8e62c69a445be5b10492a212e5fa4b01277d2189
Kronos3 <dovakhiin1359@gmail.com>
2016-07-23 17:45:44 -0400

    Removed @ symbols on dictionary

--- src/cglobal/dictionary.h
+++ src/cglobal/dictionary.h
@@ -1,9 +1,9 @@
 
 /*-------------------------------------------------------------------------*/
 /**
-   @file    dictionary.h
-   @author  N. Devillard
-   @brief   Implements a dictionary for string variables.
+   file    dictionary.h
+   author  N. Devillard
+   brief   Implements a dictionary for string variables.
 
    This module implements a simple dictionary object, i.e. a list
    of string/string associations. This object is useful to store e.g.
@@ -34,7 +34,7 @@ extern "C" {
 
 /*-------------------------------------------------------------------------*/
 /**
-  @brief    Dictionary object
+  brief    Dictionary object
 
   This object contains a list of string/string associations. Each
   association is identified by a unique string key. Looking up values
@@ -57,9 +57,9 @@ typedef struct _dictionary_ {
 
 /*-------------------------------------------------------------------------*/
 /**
-  @brief    Compute the hash key for a string.
-  @param    key     Character string to use for key.
-  @return   1 unsigned int on at least 32 bits.
+  brief    Compute the hash key for a string.
+  param    key     Character string to use for key.
+  return   1 unsigned int on at least 32 bits.
 
   This hash function has been taken from an Article in Dr Dobbs Journal.
   This is normally a collision-free function, distributing keys evenly.
@@ -71,9 +71,9 @@ unsigned dictionary_hash(const char * key);
 
 /*-------------------------------------------------------------------------*/
 /**
-  @brief    Create a new dictionary object.
-  @param    size    Optional initial size of the dictionary.
-  @return   1 newly allocated dictionary objet.
+  brief    Create a new dictionary object.
+  param    size    Optional initial size of the dictionary.
+  return   1 newly allocated dictionary objet.
 
   This function allocates a new dictionary object of given size and returns
   it. If you do not know in advance (roughly) the number of entries in the
@@ -84,9 +84,9 @@ dictionary * dictionary_new(size_t size);
 
 /*-------------------------------------------------------------------------*/
 /**
-  @brief    Delete a dictionary object
-  @param    d   dictionary object to deallocate.
-  @return   void
+  brief    Delete a dictionary object
+  param    d   dictionary object to deallocate.
+  return   void
 
   Deallocate a dictionary object and all memory associated to it.
  */
@@ -95,11 +95,11 @@ void dictionary_del(dictionary * vd);
 
 /*-------------------------------------------------------------------------*/
 /**
-  @brief    Get a value from a dictionary.
-  @param    d       dictionary object to search.
-  @param    key     Key to look for in the dictionary.
-  @param    def     Default value to return if key not found.
-  @return   1 pointer to internally allocated character string.
+  brief    Get a value from a dictionary.
+  param    d       dictionary object to search.
+  param    key     Key to look for in the dictionary.
+  param    def     Default value to return if key not found.
+  return   1 pointer to internally allocated character string.
 
   This function locates a key in a dictionary and returns a pointer to its
   value, or the passed 'def' pointer if no such key can be found in
@@ -112,11 +112,11 @@ const char * dictionary_get(const dictionary * d, const char * key, const char *
 
 /*-------------------------------------------------------------------------*/
 /**
-  @brief    Set a value in a dictionary.
-  @param    d       dictionary object to modify.
-  @param    key     Key to modify or add.
-  @param    val     Value to add.
-  @return   int     0 if Ok, anything else otherwise
+  brief    Set a value in a dictionary.
+  param    d       dictionary object to modify.
+  param    key     Key to modify or add.
+  param    val     Value to add.
+  return   int     0 if Ok, anything else otherwise
 
   If the given key is found in the dictionary, the associated value is
   replaced by the provided one. If the key cannot be found in the
@@ -140,10 +140,10 @@ int dictionary_set(dictionary * vd, const char * key, const char * val);
 
 /*-------------------------------------------------------------------------*/
 /**
-  @brief    Delete a key in a dictionary
-  @param    d       dictionary object to modify.
-  @param    key     Key to remove.
-  @return   void
+  brief    Delete a key in a dictionary
+  param    d       dictionary object to modify.
+  param    key     Key to remove.
+  return   void
 
   This function deletes a key in a dictionary. Nothing is done if the
   key cannot be found.
@@ -154,13 +154,13 @@ void dictionary_unset(dictionary * d, const char * key);
 
 /*-------------------------------------------------------------------------*/
 /**
-  @brief    Dump a dictionary to an opened file pointer.
-  @param    d   Dictionary to dump
-  @param    f   Opened file pointer.
-  @return   void
+  brief    Dump a dictionary to an opened file pointer.
+  param    d   Dictionary to dump
+  param    f   Opened file pointer.
+  return   void
 
   Dumps a dictionary onto an opened file pointer. Key pairs are printed out
-  as @c [Key]=[Value], one per line. It is Ok to provide stdout or stderr as
+  as c [Key]=[Value], one per line. It is Ok to provide stdout or stderr as
   output file pointers.
  */
 /*--------------------------------------------------------------------------*/
