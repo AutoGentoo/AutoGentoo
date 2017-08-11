d2bd57c8a65aec4fd1279adbae5b19d2f41aaa5a
Kronos3 <dovakhiin1359@gmail.com>
2017-08-11 10:37:17 -0400

    Add start files for AM

---
+++ src/htoip_port.c
@@ -0,0 +1,62 @@
+/*
+ * htoip_port.c
+ * 
+ * Copyright 2017 Unknown <atuser@Hyperion>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <htoip_port.h>
+
+int hostname_to_ip (char * hostname, char * ip) {
+    struct hostent *he;
+    struct in_addr **addr_list;
+    int i;
+         
+    if ( (he = gethostbyname( hostname ) ) == NULL) 
+    {
+        // get the host info
+        herror("gethostbyname");
+        return 1;
+    }
+ 
+    addr_list = (struct in_addr **) he->h_addr_list;
+     
+    for(i = 0; addr_list[i] != NULL; i++) 
+    {
+        //Return the first one;
+        strcpy(ip , inet_ntoa(*addr_list[i]) );
+        return 0;
+    }
+     
+    return 1;
+}
+
+static PyObject *
+hostnameToIp (PyObject * self, PyObject * args) {
+    char * hostname;
+    char ip [100];
+    
+    if (!PyArg_ParseTuple (args, "s", &hostname) {
+        return NULL;
+    }
+    hostname_to_ip(hostname , ip);
+    
+    return Py_BuildValue("s", ip);
+}
