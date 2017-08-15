f3497148da65d1eeaba1cc286ade00b4a43d305f
Kronos3 <dovakhiin1359@gmail.com>
2017-08-14 22:26:53 -0400

    Implemented main

---
+++ include/autogentoo.h
@@ -0,0 +1,50 @@
+/*
+ * autogentoo.h
+ * 
+ * Copyright 2017 Unknown <atuser@Hyperion>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#ifndef __AUTOGENTOO__
+#define __AUTOGENTOO__
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <serve_client.h>
+#include <server.h>
+#include <ip_convert.h>
+
+struct server_config {
+    char ip[32]; // Will always be this hosts ip
+    char port[4]; // 9490
+    char root[256]; // Same root as in serve_client_manager
+};
+
+struct AutoGentoo {
+    struct serve_client_manager manager;
+    struct server_config cfg_server;
+    char * config_path;
+};
+
+extern struct AutoGentoo config_m;
+void _getcwd (char* out, size_t size);
+
+#endif
--- src/autogentoo.c
+++ src/autogentoo.c
@@ -20,26 +20,8 @@
  * 
  * 
  */
-
-
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <serve_client.h>
-#include <server.h>
-
-struct server_config {
-    char* ip; // Will always be this hosts ip
-    char* port; // 9490
-    char* root; // Same root as in serve_client_manager
-};
-
-struct AutoGentoo {
-    struct serve_client_manager * manager;
-    struct server_config cfg_server;
-    int configfd;
-}
+ 
+#include <autogentoo.h>
 
 struct AutoGentoo config_m;
 
@@ -49,14 +31,16 @@ void _getcwd (char* out, size_t size) {
 }
 
 int main (int argc, char ** argv) {
-    char* b_root;
-    char* b_fconfig;
+    char b_root[128];
+    _getcwd (b_root, sizeof (b_root));
+    char* b_fconfig = ".autogentoo.config";
     char  use_daemon = 0;
+    int c;
     
     while ((c = getopt (argc, argv, "r:c:d")) != -1) {
         switch (c) {
             case 'r':
-                b_root = optarg;
+                strcpy(b_root, optarg);
                 break;
             case 'c':
                 b_fconfig = optarg;
@@ -75,22 +59,34 @@ int main (int argc, char ** argv) {
                        optopt);
                 return 1;
             default:
-                _getcwd (b_root, sizeof (b_root));
-                b_fconfig = ".autogentoo.config";
+                break;
         }
     }
     
     char b_hs [32];
     gethostname (b_hs, sizeof (b_hs));
-    config_m = {
-        init_manager (b_root),
-        {
-            get_ip_from_hostname (b_hs)
-            "9490",
-            b_root
-        },
-        open (b_fconfig, "a");
+    
+    struct serve_client_manager manager_t;
+    
+    if (access (b_fconfig, F_OK) != -1) {
+        FILE * __fd = fopen (b_fconfig, "rb");
+        int _fd = fileno(__fd);
+        read_manager (_fd, &manager_t);
+        fclose (__fd);
     }
+    else {
+        fprintf (stderr, "Config %s not found, creating new\n", b_fconfig);
+        printf ("Creating manager in %s\n", b_root);
+        fflush(stdout);
+        manager_t = init_manager (b_root);
+    }
+    
+    hostname_to_ip (b_hs, config_m.cfg_server.ip);
+    strcpy(config_m.cfg_server.port, "9490");
+    strcpy(config_m.cfg_server.root, manager_t.top_dir);
+    
+    config_m.manager = manager_t;
+    config_m.config_path = b_fconfig;
     
     return 0;
 }
