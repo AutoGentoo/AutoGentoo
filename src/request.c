88ec24719ec10cc46892b1e1a72eab17d75c2402
Kronos3 <dovakhiin1359@gmail.com>
2017-08-23 09:50:06 -0400

    Switch IDE to kronos

--- src/request.c
+++ src/request.c
@@ -81,6 +81,7 @@ response_t exec_method (request_t type, struct manager * man, char* command, int
             if (client_no < 0) {
                 return UNAUTHORIZED;
             }
+            printf ("%s\n", man->clients[client_no].hostname);
             return methods[i].method (command, man, man->clients[client_no]);
         }
     }
