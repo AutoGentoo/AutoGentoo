0992fbda756ab102be45bfe7d214a7cdc0faa8c0
Kronos3 <dovakhiin1359@gmail.com>
2017-09-16 01:33:07 -0400

    Remove redirection

--- src/server.c
+++ src/server.c
@@ -92,7 +92,7 @@ void server_respond (int n, struct manager * m_man)
     response_t res;
 
     memset((void*)mesg, (int)'\0', 2048);
-
+    /*
     // Create buffs to redirect STDOUT and STDERR
     int stdout_b, stderr_b;
     stdout_b = dup (STDOUT_FILENO);
@@ -100,7 +100,7 @@ void server_respond (int n, struct manager * m_man)
     dup2(clients[n], STDOUT_FILENO);
     dup2(clients[n], STDERR_FILENO);
     close(clients[n]);
-    
+    */
     rcvd = recv(clients[n], mesg, 2048, 0);
     int __error = 0;
     if (rcvd < 0) { // receive error
@@ -470,12 +470,12 @@ void server_respond (int n, struct manager * m_man)
         }
     }
 
-
+    /*
     close (STDOUT_FILENO);
     close (STDERR_FILENO);
     dup2 (stdout_b, STDOUT_FILENO); // Restore stdout/stderr to terminal
     dup2 (stderr_b, STDERR_FILENO);
-
+    */
     shutdown(
         clients[n],
         SHUT_RDWR); // All further send and recieve operations are DISABLED...
