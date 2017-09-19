95b520584fee79e77698d2a27f45eaa1db08eaf9
Kronos3 <dovakhiin1359@gmail.com>
2017-09-19 17:02:56 -0400

    Fixed args for execve

--- src/request.c
+++ src/request.c
@@ -61,6 +61,7 @@ response_t m_install (char* command, struct manager * m_man, int sc_no, char* ip
     strcpy (root, path_normalize (root));
     
     char *args[] = {
+        "chroot",
         root,
         "/usr/bin/emerge",
         command,
