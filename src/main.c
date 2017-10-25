6dc28c625719e8efcf65a7d91b23e09a0bb73733
Kronos3 <dovakhiin1359@gmail.com>
2017-10-24 20:33:06 -0400

    Made one binary (removed lib

--- src/main.c
+++ src/main.c
@@ -15,16 +15,5 @@ int main() {
     printf("PORTDIR = %s\n", conf_get(test_config, NULL, "PORTDIR"));
     conf_free(test_config);
 
-    Conf* test_repo = conf_new("/etc/portage/repos.conf/rrr");
-    printf("DEFAULT.main-repo = %s\n", conf_get(test_repo, "DEFAULT", "main-repo"));
-    //print_config(test_repo);
-    conf_free(test_repo);
-
-    RepoConfig* configrepo = repo_config_new();
-    repo_config_read (configrepo, "/etc/portage/repos.conf/rrr");
-    printf ("repo1->name: %s\n", (*(Repository**)vector_get(configrepo->repositories, 0))->name);
-    printf ("repo1->sync-uri: %s\n", (*(Repository**)vector_get(configrepo->repositories, 0))->sync_uri);
-    repo_config_free(configrepo);
-
     return 0;
 }
