cda5d31d67ffe89804a79c4f55b209a03e9e7c7b
Kronos3 <dovakhiin1359@gmail.com>
2017-11-27 16:42:37 -0500

    Added chroot and began to implement

--- include/chroot.h
+++ include/chroot.h
@@ -5,11 +5,22 @@ typedef struct __Chroot Chroot;
 typedef struct __ChrootMount ChrootMount;
 
 typedef enum {
-    
+    NO_RECURSIVE,
+    RECURSIVE
 } mount_t;
 
+typedef enum {
+    FAILED = 0x0,
+    PORTAGE = 0x1,
+    SYS = 0x2,
+    DEV = 0x4,
+    PROC = 0x8
+} mounts_t;
+
 struct __Chroot {
-    
+    Host* parent;
+    Vector* mounts;
+    mounts_t status;
 };
 
 struct __ChrootMount {
@@ -18,5 +29,9 @@ struct __ChrootMount {
     char* type; // NULL for default
     mount_t opts;
 }
+
+response_t enter (Chroot* chroot, (response_t*)(void*));
+Chroot* chroot_new (Host* parent);
+mounts_t chroot_mount (Chroot* chroot);
 
 #endif
---
+++ src/chroot.c
@@ -0,0 +1,13 @@
+#include <chroot.h>
+
+response_t enter (Chroot* chroot, (response_t*)(void*)) {
+    
+}
+
+Chroot* chroot_new (Host* parent) {
+    
+}
+
+mounts_t chroot_mount (Chroot* chroot) {
+    
+}
