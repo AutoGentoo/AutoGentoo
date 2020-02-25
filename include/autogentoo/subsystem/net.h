3adb5fd6fa79f88ac4d400c6929f4b88af91d2da
Kronos3 <andreitumbar@gmail.com>
2020-02-25 14:45:35 -0500

    Common bridge target on NS side

--- include/autogentoo/subsystem/net.h
+++ include/autogentoo/subsystem/net.h
@@ -20,21 +20,29 @@ struct __NetworkInterface {
 	char* addr_command;
 	char* addr_command_child;
 	
-	char* name_parent; /* veth[interface_n] * 2 */
-	char* name_child; /* veth[interface_n] * 2 + 1 */
+	char* name_parent; /* veth[interface_n] */
+	char* name_child; /* eth0 */
 	
-	char* address_parent; /* 10.0.0.[2 * (interface_n + 1)] */
-	char* address_child; /* 10.0.0.[2 * (interface_n + 1) + 1] */
+	char* address_parent; /* 10.0.0.[2 * interface_n] */
+	char* address_child; /* 10.0.0.[2 * interface_n + 1] */
 };
 
 NetworkInterface* network_interface_new(pid_t pid);
 
+
+
 /**
- * Run the parent initialization for 
+ * Run the parent initialization for the network interface
+ * @param ni interface to perform a parent init on
+ * @return error code (0 for success)
+ */
+int network_interface_pinit(NetworkInterface* ni);
+
+/**
+ * Run the child initialization
  * @param ni
  * @return
  */
-int network_interface_pinit(NetworkInterface* ni);
 int network_interface_cinit(NetworkInterface* ni);
 
 #endif //AUTOGENTOO_NET_H
