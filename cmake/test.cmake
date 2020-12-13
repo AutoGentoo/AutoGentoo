a171f6755624a98c8e10eaf12e85ac2fba748801
Andrei Tumbar <andreitumbar@gmail.com>
2020-12-13 12:52:58 -0500

    Fix parser reduce conflicts

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -3,15 +3,6 @@ function(setup_cmocka)
     include(${CMAKE_SOURCE_DIR}/cmake/AddMockedTest.cmake)
 endfunction()
 
-set(CMAKE_DISABLED_TESTS ${CMAKE_DISABLED_TESTS})
-
-macro(set_tests_properties target)
-    if (${target} IN_LIST CMAKE_DISABLED_TESTS)
-    else()
-        _set_tests_properties(${ARGV})
-    endif()
-endmacro()
-
 setup_cmocka()
 enable_testing()
 add_subdirectory(${CMAKE_SOURCE_DIR}/autogentoo/test)
